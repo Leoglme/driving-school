319529f88bb747e3c8fabc3b9f42621265657bbd
Léo Guillaume <leo.guillaume@gmail.com>
2022-06-28 22:40:12 +0200

    delete and update user

--- app/routes/user.py
+++ app/routes/user.py
@@ -1,4 +1,3 @@
-import json
 from .index import router
 from flask import request
 from .. import db
@@ -6,30 +5,7 @@ from ..models import User
 from flask import jsonify
 
 
-@router.route('/users', methods=['GET'])
-def index():
-    users = User.query.all()
-    return jsonify(User.serialize_list(users))
-
-
-@router.route('/user/<int:user_id>', methods=['GET'])
-def show(user_id):
-    user = User.query.filter_by(id=user_id).first()
-    if user:
-        return jsonify(User.serialize(user))
-    return f"User with id {user_id} doesn't exist"
-
-
-@router.route('/user', methods=['PUT'])
-def update():
-    return 'user created', 201
-
-
-@router.route('/user', methods=['DELETE'])
-def destroy():
-    return 'user created', 201
-
-
+# Create user
 @router.route('/user', methods=['POST'])
 def store():
     payload = request.get_json()
@@ -41,4 +17,46 @@ def store():
     user = User(email=email, password=password, first_name=first_name, last_name=last_name, role_id=role_id)
     db.session.add(user)
     db.session.commit()
-    return 'user created', 201
+    return 'User created', 201
+
+
+# Get all users
+@router.route('/users', methods=['GET'])
+def index():
+    users = User.query.all()
+    return jsonify(User.serialize_list(users))
+
+
+# Get user by id
+@router.route('/user/<int:user_id>', methods=['GET'])
+def show(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        return jsonify(User.serialize(user))
+    return f"User with id {user_id} doesn't exist"
+
+
+# Update user
+@router.route('/user/<int:user_id>', methods=['PUT'])
+def update(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        payload = request.get_json()
+        user.email = payload['email']
+        user.first_name = payload['first_name']
+        user.last_name = payload['last_name']
+        user.role_id = payload['role_id']
+        db.session.commit()
+        return 'User Updated'
+    return f"User with id {user_id} doesn't exist"
+
+
+# Delete user
+@router.route('/user/<int:user_id>', methods=['DELETE'])
+def destroy(user_id):
+    user = User.query.filter_by(id=user_id).first()
+    if user:
+        db.session.delete(user)
+        db.session.commit()
+        return 'User deleted'
+    return f"User with id {user_id} doesn't exist"
